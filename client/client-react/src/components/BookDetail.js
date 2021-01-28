77ce250727d1877db5491f8ee65a3cc028b8b1e8
phanvinhquy08 <quy.pv3@neo-lab.vn>
2021-01-28 17:40:05 +0700

    update notice

--- client/client-react/src/components/BookDetail.js
+++ client/client-react/src/components/BookDetail.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react'
-import { useMutation, useQuery } from '@apollo/client';
+import { useMutation, useQuery, NetworkStatus } from '@apollo/client';
 
 import { getBookQuery } from '../queries/queries';
 
 const BookDetail = props => {
-    const { loading, error, data } = useQuery(getBookQuery, { variables: { id : props.id }});
+    console.log('render');
+    const { loading, error, data, refetch,networkStatus } = useQuery(getBookQuery, { variables: { id : props.id }, notifyOnNetworkStatusChange: true, errorPolicy: 'none'});
+    
+    if(networkStatus === NetworkStatus.refetch) {
+        return (
+            <div>Refetch !!! </div>
+        )
+    }
+
+    console.log(error);
     return (
         <div className="book-detail">
             {loading && <div>Loading...</div>}
             {data && (
                 <div>
-                    <p>{data.book.name}</p>
+                    <p>{data.book.name}</p><button onClick={() => refetch()}>Re fetch</button>
                     <p>{data.book.author.name}</p>
                 </div>
             )}
